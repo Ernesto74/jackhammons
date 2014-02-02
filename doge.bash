844098d174d6638f851afa3a1622adcd3ac609ba
jackchammons <jackchammons@gmail.com>
2014-02-01 20:01:51 -0500

    doge.bash first commit

---
+++ doge.bash
@@ -0,0 +1,21 @@
+#!/bin/bash  
+
+sudo apt-get update
+
+sudo apt-get install build-essential libcurl4-openssl-dev
+
+wget http://sourceforge.net/projects/cpuminer/files/pooler-cpuminer-2.3.2.tar.gz
+
+tar -xzf pooler-cpuminer-2.3.2.tar.gz
+
+cd cpuminer-2.3.2/
+
+./configure CFLAGS="-O3"
+
+make
+
+apt-get install screen
+
+screen
+
+./minerd --url=stratum+tcp://teamdoge.com:3333 --userpass=jackchammons.user:password
